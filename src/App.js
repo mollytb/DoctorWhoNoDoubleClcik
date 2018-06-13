c7ffbfc7a31919d58f745058c40e62a2623b58aa
Molly Burns <Molly@Kathryns-MBP.attlocal.net>
2018-06-13 17:18:30 -0400

    rotate and scoring added

--- src/App.js
+++ src/App.js
@@ -1,4 +1,6 @@
-import React, { Component } from "react";
+import React, {
+  Component
+} from "react";
 import DoctorCard from "./components/DoctorCard";
 import Wrapper from "./components/Wrapper";
 import Title from "./components/Title";
@@ -8,35 +10,69 @@ import "./App.css";
 class App extends Component {
   // Setting this.state.friends to the friends json array
   state = {
-    doctors
-  };
+    message: "Click an image to begin!",
+    topScore: 0,
+    currentScore: 0,
+    doctors: doctors,
+    unselectedDoctors: doctors
 
-  rotateDoctors = id => {
-    // Filter this.state.friends for friends with an id not equal to the id being removed
-    const doctors = this.state.doctors.filter(doctor => doctor.id !== id);
-    // Set this.state.friends equal to the new friends array
-    this.setState({ doctors });
   };
+  componentDidMount() {}
+  clickDoctorCard = id => {
+    const findDoctor = this.state.unselectedDoctors.find(item => item.id === id);
 
-  // Map over this.state.friends and render a FriendCard component for each friend object
-  render() {
-    return (
-      <Wrapper>
-        <Title>Doctor Who</Title>
-        {this.state.doctors.map(doctor => (
-          <DoctorCard
-          rotateDoctors={this.rotateDoctors}
-            id={doctor.id}
-            key={doctor.id}
-            name={doctor.name}
-            image={doctor.image}
-            occupation={doctor.occupation}
-           
-          />
-        ))}
-      </Wrapper>
-    );
+    if (findDoctor === undefined) {
+      // failure to select a new doctor
+      this.setState({
+        message: "You guessed incorrectly!",
+        topScore: (this.state.currentScore > this.state.topScore) ? this.state.currentScore : this.state.topScore,
+        currentScore: 0,
+        doctors: doctors,
+        unselectedDoctors: doctors
+
+      });
+    }
+      else {
+        // success to select a new doctor
+        const newDoctors = this.state.unselectedDoctors.filter(item => item.id !== id);
+
+        this.setState({
+          message: "You guessed correctly!",
+          currentScore: this.state.currentScore + 1,
+          doctors: doctors,
+          unselectedDoctors: doctors
+        });
+      }
+      this.rotateDoctors(doctors);
+    };
+  
+    rotateDoctors = array => {
+      for (let i = array.length - 1; i > 0; i--) {
+        let j = Math.floor(Math.random() * (i + 1));
+        [array[i], array[j]] = [array[j], array[i]];
+      }
+    }
+
+
+    // Map over this.state.friends and render a FriendCard component for each friend object
+    render() {
+      return ( <Wrapper>
+        <Title> Doctor Who </Title> {
+          this.state.doctors.map(doctor => ( <
+            DoctorCard rotateDoctors = {
+              this.rotateDoctors
+            }
+            id = {doctor.id}
+            key = {doctor.id}
+            name = {doctor.name}
+            image = {doctor.image}
+            occupation = {doctor.occupation}
+            />
+          ))
+        } 
+        </Wrapper>
+      );
+    }
   }
-}
 
-export default App;
+  export default App;
